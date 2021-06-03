2249f7597c0fb45671aa4f7feb05217e3cf808fc
Julius-R <julius.robinson95@yahoo.com>
2021-06-03 14:52:30 -0600

    testing out routes on about

--- routes/about.js
+++ routes/about.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
+router.use("/user", (req, res, next) => {
+  if (req.body.title !== "") {
+    console.log(req.body.title);
+  } else {
+    console.log("oopsy: ", req.body.title);
+  }
+});
+
 router.get("/", (req, res) => {
   res.render("about", {
     layout: "index",
     title: "About",
+    person: "",
+  });
+});
+
+router.get("/:person", (req, res) => {
+  res.render("about", {
+    layout: "index",
+    title: "About",
+    person: req.params.person.toUpperCase(),
+  });
+});
+
+router.post("/user", (req, res) => {
+  res.json({
+    name: "Hannes",
   });
 });
 
